a9d9c803355ef6e9014883cbc0dee08939fc1275
Aniket Aryamane <aniket.aryamane@fafadiatech.com>
2023-05-19 13:56:58 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,72 @@
+import { useState } from "react";
+import AddTask from "./components/AddTask";
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+
+function App() {
+    const [showAddTask, setShowAddTask] = useState(false);
+
+    const [tasks, setTasks] = useState([
+        {
+            id: 1,
+            text: "Doctor's Appointment",
+            day: "Feb 5th at 2:30pm",
+            reminder: true,
+        },
+        {
+            id: 2,
+            text: "Meeting at School",
+            day: "Feb 6th at 1:30pm",
+            reminder: true,
+        },
+        {
+            id: 3,
+            text: "Food Shopping",
+            day: "Feb 5th at 2:30pm",
+            reminder: false,
+        },
+    ]);
+
+    const deleteTask = (id) => {
+        setTasks(tasks.filter((task) => task.id !== id));
+    };
+
+    const toggleReminder = (id) => {
+        setTasks(
+            tasks.map((task) =>
+                task.id === id ? { ...task, reminder: !task.reminder } : task
+            )
+        );
+    };
+
+    const addTask = (task) => {
+        const id = Math.floor(Math.random() * 10000) + 1;
+        const newTask = { id, ...task };
+        setTasks([...tasks, newTask]);
+    };
+
+    return (
+        <div className="container">
+            <Header
+                showAdd={showAddTask}
+                onAdd={() => {
+                    setShowAddTask(!showAddTask);
+                }}
+            />
+            {showAddTask && <AddTask onAddTask={addTask} />}
+            {tasks.length > 0 ? (
+                <Tasks
+                    tasks={tasks}
+                    onDelete={deleteTask}
+                    onToggle={toggleReminder}
+                />
+            ) : (
+                <h3 className="task" style={{ cursor: "initial" }}>
+                    Task list - Empty !
+                </h3>
+            )}
+        </div>
+    );
+}
+
+export default App;
